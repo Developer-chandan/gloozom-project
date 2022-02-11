b10b967deeedba4c08a45da958a35778f078ed77
Chandan Sen <chandan2441139@gmail.com>
2022-02-12 02:33:07 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,314 @@
+
+
+///////animation scroll/////
+///Slider animation ///
+
+$(document).ready(function(){
+  $('#showcase').waypoint(function(direction){
+    $('.button-animate').addClass('animate__animated animate__fadeInUp')
+  },{
+    offset:'10%'
+  })
+})
+
+$(document).ready(function(){
+  $('#showcase').waypoint(function(direction){
+    $('.carosel-animate-descp').addClass('animate__animated animate__fadeInDown')
+  },{
+    offset:'30%'
+  })
+})
+
+////home icon animation //
+$(document).ready(function(){
+  $('#home-icon').waypoint(function(direction){
+
+    $('.home-animation-part1').addClass('animate__animated animate__fadeInLeft')
+  },{
+    offset:'80%'
+  })
+})
+
+$(document).ready(function(){
+  $('#home-icon').waypoint(function(direction){
+    $('.home-annimatee-part2').addClass('animate__animated animate__fadeInUp')
+  },{
+    offset:'80%'
+  })
+})
+
+$(document).ready(function(){
+  $('#home-icon').waypoint(function(direction){
+    $('.home-animation-part3').addClass('animate__animated animate__fadeInRight')
+  },{
+    offset:'80%'
+  })
+})
+////get strat section//
+$(document).ready(function(){
+  $('#getstart-section').waypoint(function(direction){
+    $('.start-anition-title').addClass('animate__animated animate__fadeInDown')
+  },{
+    offset:'80%'
+  })
+})
+$(document).ready(function(){
+  $('#getstart-section').waypoint(function(direction){
+    $('.start-anition-descp').addClass('animate__animated animate__fadeInUp')
+  },{
+    offset:'80%'
+  })
+})
+///info section//////
+$(document).ready(function(){
+  $('#infoSection').waypoint(function(direction){
+    $('.info-animate-descp').addClass('animate__animated animate__fadeInLeft')
+  },{
+    offset:'80%'
+  })
+})
+$(document).ready(function(){
+  $('#infoSection').waypoint(function(direction){
+    $('.info-animate-image').addClass('animate__animated animate__fadeInRight')
+  },{
+    offset:'80%'
+  })
+})
+////crashcourse///
+$(document).ready(function(){
+  $('#crash-corse').waypoint(function(direction){
+    $('.play-animation').addClass('animate__animated animate__fadeInDown')
+  },{
+    offset:'80%'
+  })
+})
+$(document).ready(function(){
+  $('#crash-corse').waypoint(function(direction){
+    $('.header-animation').addClass('animate__animated animate__fadeInUp')
+  },{
+    offset:'80%'
+  })
+})
+///photo gallery//
+$(document).ready(function(){
+  $('#photo-gallery').waypoint(function(direction){
+    $('.photo-title-animate').addClass('animate__animated animate__fadeInDown')
+  },{
+    offset:'80%'
+  })
+})
+
+$(document).ready(function(){
+  $('#photo-gallery').waypoint(function(direction){
+    $('.first-row-image').addClass('animate__animated animate__zoomIn')
+  },{
+    offset:'70%'
+  })
+})
+
+$(document).ready(function(){
+  $('#photo-gallery').waypoint(function(direction){
+    $('.second-row-image').addClass('animate__animated animate__zoomIn')
+  },{
+    offset:'35%'
+  })
+})
+////signup ////
+$(document).ready(function(){
+  $('#signup').waypoint(function(direction){
+    $('.signup-inner').addClass('animate__animated animate__fadeInDown')
+  },{
+    offset:'60%'
+  })
+})
+$(document).ready(function(){
+  $('#signup').waypoint(function(direction){
+    $('.signup-form').addClass('animate__animated animate__zoomIn')
+  },{
+    offset:'70%'
+  })
+})
+
+
+
+////About page////////////
+///about-header-descp
+$(document).ready(function(){
+  $('#about-image').waypoint(function(direction){
+    $('.about-header-h1').addClass('animate__animated animate__fadeInDown')
+  },{
+    offset:'60%'
+  })
+})
+$(document).ready(function(){
+  $('#about-image').waypoint(function(direction){
+    $('.about-header-descp').addClass('animate__animated animate__fadeInUp')
+  },{
+    offset:'60%'
+  })
+})
+
+//infor-section///
+$(document).ready(function(){
+  $('#infor-section').waypoint(function(direction){
+    $('.information-descp').addClass('animate__animated animate__fadeInLeft')
+  },{
+    offset:'100%'
+  })
+})
+$(document).ready(function(){
+  $('#infor-section').waypoint(function(direction){
+    $('.information-image').addClass('animate__animated animate__zoomIn')
+  },{
+    offset:'100%'
+  })
+})
+
+$(document).ready(function(){
+  $('#demo-box').waypoint(function(direction){
+    $('.first-box-row-animate').addClass('animate__animated animate__zoomIn')
+  },{
+    offset:'80%'
+  })
+})
+$(document).ready(function(){
+  $('#demo-box').waypoint(function(direction){
+    $('.second-box-row-animate').addClass('animate__animated animate__zoomIn')
+  },{
+    offset:'35%'
+  })
+})
+
+//////service section
+$(document).ready(function(){
+  $('#service-image-heading').waypoint(function(direction){
+    $('.service-header-animate-h1').addClass('animate__animated animate__fadeInDown')
+  },{
+    offset:'60%'
+  })
+})
+
+
+$(document).ready(function(){
+  $('#service-image-heading').waypoint(function(direction){
+    $('.service-header-animate-descp').addClass('animate__animated animate__fadeInUp')
+  },{
+    offset:'60%'
+  })
+})
+
+///card scroll
+
+
+$(document).ready(function(){
+  $('#service-box').waypoint(function(direction){
+    $('.card-animation-service').addClass('animate__animated animate__zoomIn')
+  },{
+    offset:'80%'
+  })
+})
+
+
+/////blog page
+//header block
+
+$(document).ready(function(){
+  $('#blog-header-image').waypoint(function(direction){
+    $('.blog-title-animate').addClass('animate__animated animate__fadeInDown')
+  },{
+    offset:'60%'
+  })
+})
+
+
+$(document).ready(function(){
+  $('#blog-header-image').waypoint(function(direction){
+    $('.blog-descp-animate').addClass('animate__animated animate__fadeInUp')
+  },{
+    offset:'60%'
+  })
+})
+
+////blog-box-animate
+$(document).ready(function(){
+  $('#blog-box').waypoint(function(direction){
+    $('.blog-box-animate').addClass('animate__animated animate__zoomIn')
+  },{
+    offset:'80%'
+  })
+})
+
+
+/////conact page
+
+//conatact header animate
+
+$(document).ready(function(){
+  $('#contact-header-image').waypoint(function(direction){
+    $('.contact-title-animate').addClass('animate__animated animate__fadeInDown')
+  },{
+    offset:'60%'
+  })
+})
+
+$(document).ready(function(){
+  $('#contact-header-image').waypoint(function(direction){
+    $('.contact-descp-animate').addClass('animate__animated animate__fadeInUp')
+  },{
+    offset:'60%'
+  })
+})
+
+////contact boxes
+
+
+
+$(document).ready(function(){
+  $('#contact-boxes').waypoint(function(direction){
+    $('.contact-box-up-animate').addClass('animate__animated animate__fadeInDownBig')
+  },{
+    offset:'60%'
+  })
+})
+
+$(document).ready(function(){
+  $('#contact-boxes').waypoint(function(direction){
+    $('.contact-box-left-animate').addClass('animate__animated animate__fadeInLeft')
+  },{
+    offset:'60%'
+  })
+})
+
+$(document).ready(function(){
+  $('#contact-boxes').waypoint(function(direction){
+    $('.contact-box-right-animate').addClass('animate__animated animate__fadeInRight')
+  },{
+    offset:'60%'
+  })
+})
+
+$(document).ready(function(){
+  $('#contact-boxes').waypoint(function(direction){
+    $('.contact-box-down-animate').addClass('animate__animated animate__fadeInUp')
+  },{
+    offset:'60%'
+  })
+})
+
+$(document).ready(function(){
+  $('#contact-boxes').waypoint(function(direction){
+    $('.contact-zoomin-animate').addClass('animate__animated animate__zoomIn')
+  },{
+    offset:'60%'
+  })
+})
+
+
+
+//////////
+$('.slider').slick({
+  infinite:true,
+  slideToShow:1,
+  slideToScroll:1
+
+})
